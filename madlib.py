c8f3a7cc0a1ae50f9ee2946c56fc84016aec99e6
bishantkc <151637867+bishantkc@users.noreply.github.com>
2023-12-06 15:55:14 +0545

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,14 @@
+# youtuber = "Bishant"
+# print(f"Subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+adj2 = input("Adjective: ")
+verb = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = (f"Coding is very {adj}! It makes me feel very relax all the time because \n"
+          f"I feel coding very {adj2}. Practice coding and {verb} like you are {famous_person}! ")
+
+print(madlib)
+
+
