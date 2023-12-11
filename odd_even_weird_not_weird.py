ac07d51eb2af82f5aee6137141eea98d9919143f
bishantkc <151637867+bishantkc@users.noreply.github.com>
2023-12-11 23:09:42 +0545

    Update odd_even_weird_not_weird.py

--- odd_even_weird_not_weird.py
+++ odd_even_weird_not_weird.py
@@ -2,7 +2,7 @@
 def odd_even(n):
     if n % 2 != 0:
         print("Weird")
-    elif n % 2 == 0 in range(2,5):
+    elif n % 2 == 0 in range(2,5): # this means number is even
         print("Not Weird")
     elif n % 2 == 0 in range(6,20):
         print("Weird")
